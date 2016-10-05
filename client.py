b5acffbdfe76496a82a8117c19ce0b737798c202
tsh <dr.tallin@gmail.com>
2016-10-05 18:18:15 +0300

    oop for basic render

--- client.py
+++ client.py
@@ -1,34 +1,32 @@
 import asyncio
 import tkinter as tk
 
-import arcade
-import websockets
+# import arcade
+# import websockets
+
+from client_objs import BaseObject
 
 
 # GRAPHICS
+
 root = tk.Tk()
 canvas = tk.Canvas(root, width=200, height=200, borderwidth=0, highlightthickness=0, bg="black")
 canvas.grid()
-
-def _create_circle(self, x, y, r, **kwargs):
-    return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
-tk.Canvas.create_circle = _create_circle
-
-canvas.create_circle(100, 120, 50, fill="blue", outline="#DDD", width=4)
+BaseObject().render(canvas)
 root.mainloop()
 
 
 # NETWORK
 
-async def hello():
-    async with websockets.connect('ws://localhost:8765') as websocket:
-
-        name = input("What's your name? ")
-        await websocket.send(name)
-        print("> {}".format(name))
-
-        greeting = await websocket.recv()
-        print("< {}".format(greeting))
+# async def hello():
+#     async with websockets.connect('ws://localhost:8765') as websocket:
+# 
+#         name = input("What's your name? ")
+#         await websocket.send(name)
+#         print("> {}".format(name))
+# 
+#         greeting = await websocket.recv()
+#         print("< {}".format(greeting))
 
 
 # if __name__ == "__main__":
---
+++ client_objs.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+class BaseObject(object):
+    def __init__(self, x=100, y=120, r=50,):
+        self.x = x
+        self.y = y
+        self.r = r
+
+    def render(self, canvas):
+        canvas.create_oval(self.x-self.r, self.y-self.r, self.x+self.r, self.y+self.r, fill="blue", outline="#DDD", width=4)
