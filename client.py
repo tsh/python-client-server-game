eb47f952651387a2eaba45e3e38b135035e58841
tsh <dr.tallin@gmail.com>
2016-10-10 00:05:21 +0300

    use asyncio with tk

--- client.py
+++ client.py
@@ -10,25 +10,28 @@ from client_objs import BaseObject
 # GRAPHICS
 
 root = tk.Tk()
-canvas = tk.Canvas(root, width=200, height=200, borderwidth=0, highlightthickness=0, bg="black")
+canvas = tk.Canvas(root, width=800, height=600, borderwidth=0, highlightthickness=0, bg="black")
 canvas.grid()
 BaseObject().render(canvas)
-root.mainloop()
+
+@asyncio.coroutine
+def run_tk(root, interval=0.05):
+    '''
+    Run a tkinter app in an asyncio event loop.
+    '''
+    try:
+        while True:
+            root.update()
+            yield from asyncio.sleep(interval)
+    except tk.TclError as e:
+        if "application has been destroyed" not in e.args[0]:
+            raise
 
 
-# NETWORK
-
-# async def hello():
-#     async with websockets.connect('ws://localhost:8765') as websocket:
-# 
-#         name = input("What's your name? ")
-#         await websocket.send(name)
-#         print("> {}".format(name))
-# 
-#         greeting = await websocket.recv()
-#         print("< {}".format(greeting))
+def main():
+    yield from run_tk(root)
 
 
-# if __name__ == "__main__":
-    # asyncio.get_event_loop().run_until_complete(hello())
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
 
