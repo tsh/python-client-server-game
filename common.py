66018fd9f5337d360e0963c19896903c5d01b52e
tsh <dr.tallin@gmail.com>
2016-11-09 21:21:46 +0200

    move Position to common

---
+++ common.py
@@ -0,0 +1,18 @@
+class NotInGameField(Exception):
+    pass
+
+
+class Position(object):
+    """ Represent tile object position """
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __hash__(self):
+        return hash((self.x, self.y,))
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self):
+        return '({} {})'.format(self.x, self.y)
--- turn_based.py
+++ turn_based.py
@@ -2,25 +2,8 @@ import math
 import pyglet
 from pyglet.window import mouse
 
+from common import Position, NotInGameField
 
-class NotInGameField(Exception):
-    pass
-
-
-class Position(object):
-    """ Represent tile object position """
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __hash__(self):
-        return hash((self.x, self.y,))
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __repr__(self):
-        return '({} {})'.format(self.x, self.y)
 
 
 class Drawable(object):
